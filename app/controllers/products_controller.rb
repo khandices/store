c6100073b8b193f6facda2e798b7f8575a758a72
khandices <s.khandice@gmail.com>
2026-03-05 16:53:12 -0500

    Update view for create new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,4 +24,16 @@ class ProductsController < ApplicationController
     def product_params
       params.expect(product: [ :name ])
     end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+      else render :edit, status: :unprocessable_entity
+    end
+  end
 end
